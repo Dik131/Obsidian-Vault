09057fcf1a5b08317b23056c11437bf3421a5cfe
Dik131 <d.i.k.93@mail.ru>
2025-05-23 13:27:04 +0300

    vault backup: 2025-05-23 13:27:04

--- Expo Harvard/code/lecture-00/1-types.js
+++ Expo Harvard/code/lecture-00/1-types.js
@@ -1,1 +1,5 @@
-const x = 42
+const x = 42 //неявно присваиваем переменной x тип Number 
+const log = console.log //чтобы не писать console.log каждый раз
+
+log(typeof x) //вернёт number - тип переменной x
+
